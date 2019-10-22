b89a41974a201630f116de2d087974ab4c56f773
ReyanDev <reyana015@gmail.com>
2019-10-22 12:19:26 +0500

    First Commet

---
+++ app/src/main/java/com/example/commuterfamily/LoginSignupOptionActivity.java
@@ -0,0 +1,32 @@
+package com.example.commuterfamily;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginSignupOptionActivity extends AppCompatActivity {
+
+    private Button login,signup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_signup_option);
+        login=findViewById(R.id.loginCF);
+        signup=findViewById(R.id.signuppCF);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginSignupOptionActivity.this,CFLoginActivity.class));
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginSignupOptionActivity.this,CFSignUp.class));
+            }
+        });
+    }
+}
