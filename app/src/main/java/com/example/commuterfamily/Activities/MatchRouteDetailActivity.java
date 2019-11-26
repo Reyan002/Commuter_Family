0f4e70f6908f531ee82ddb99a7685abb9779db3b
Talha Khan <talhakhanswe89@gmail.com>
2019-11-26 13:01:23 +0500

    Talha Commited

---
+++ app/src/main/java/com/example/commuterfamily/Activities/MatchRouteDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.commuterfamily.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.commuterfamily.R;
+
+public class MatchRouteDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_route_detail);
+    }
+}
