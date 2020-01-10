cafda448cac028bcf299e008ce5cbc2975ff2c81
Talha Khan <talhakhanswe89@gmail.com>
2020-01-10 14:34:45 +0500

    Talha Commited

--- app/src/main/java/com/example/commuterfamily/DashBoardDrawerActivity/DashboardDrawerActivity.java
+++ app/src/main/java/com/example/commuterfamily/DashBoardDrawerActivity/DashboardDrawerActivity.java
@@ -1,23 +1,27 @@
 package com.example.commuterfamily.DashBoardDrawerActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.commuterfamily.Activities.DriveActivity;
 import com.example.commuterfamily.Activities.Notification;
 import com.example.commuterfamily.Activities.RiderRouteActivity;
+import com.example.commuterfamily.Activities.SplashScreenActivity;
 import com.example.commuterfamily.DashBoardDrawerActivity.ui.HomeFragment;
 import com.example.commuterfamily.R;
 
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.commuterfamily.SessionManager.SessionManager;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -31,6 +35,7 @@ import android.widget.Toast;
 public class DashboardDrawerActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    SessionManager sessionManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +57,7 @@ public class DashboardDrawerActivity extends AppCompatActivity {
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_drive, R.id.nav_passenger,
-                R.id.nav_wallet, R.id.nav_about)
+                R.id.nav_wallet, R.id.nav_about, R.id.nav_logout)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
@@ -81,6 +86,9 @@ public class DashboardDrawerActivity extends AppCompatActivity {
                         break;
                     case R.id.nav_about:
                         Toast.makeText(DashboardDrawerActivity.this, "About Us Activity work in progress...!!!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_logout:
+                        showPopup();
                         break;
                 }
                 return false;
@@ -112,6 +120,30 @@ public class DashboardDrawerActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+    private void showPopup() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(DashboardDrawerActivity.this);
+        alert.setMessage("Are you sure?")
+                .setPositiveButton("Logout", new DialogInterface.OnClickListener()                 {
+
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        logout();
+
+                    }
+                }).setNegativeButton("Cancel", null);
+
+        AlertDialog alert1 = alert.create();
+        alert1.show();
+    }
+
+    private void logout() {
+
+        sessionManager.logoutUser();
+        Toast.makeText(DashboardDrawerActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(DashboardDrawerActivity.this, SplashScreenActivity.class));
+    }
+
 //    public void displaySelectedScreen(int itemId){
 //
 //        Fragment fragment = null;
