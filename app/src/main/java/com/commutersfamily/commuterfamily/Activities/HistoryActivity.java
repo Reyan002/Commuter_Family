c0c0f1cebfc29c81cab5c57ec3df65a3c5bcde67
ReyanDev <reyana015@gmail.com>
2020-02-24 11:07:39 +0500

    Reyan Commit

---
+++ app/src/main/java/com/commutersfamily/commuterfamily/Activities/HistoryActivity.java
@@ -0,0 +1,16 @@
+package com.commutersfamily.commuterfamily.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.commutersfamily.commuterfamily.R;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
